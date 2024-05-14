9a06a371dce3bd0594574324355b7abcfbf4bfce
Thiago Vieira Ramos <thiago.vramos@hotmail.com>
2024-05-13 22:31:59 -0300

    ## New Changes - Added new methods for root query. - Added new methods for searching for children on the right and children on the left.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BinaryTree asn = new BinaryTree();
+
+        asn.insert(8);
+
+        System.out.println("Source"); 
+        System.out.println(asn.getSource().getvalues());
+
+        asn.insert(10);
+        asn.insert(3);
+
+        System.out.println("Left-child");
+        System.out.println(asn.getSource().getleftNode().getvalues());
+
+        System.out.println("Right-Child");
+        System.out.println(asn.getSource().getrightNode().getvalues());
+        
+        asn.insert(6);
+        asn.insert(14);
+        asn.insert(1);
+
+        asn.insert(13);
+        asn.insert(7);
+        asn.insert(4); 
     }
 }
--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -5,11 +5,35 @@ public class BinaryTree {
         this.source = null;
     }
 
-    public Node getRaiz() {
+    public Node getSource() {
         return source;
     }
 
-    public void setRaiz(Node source) {
+    public void setSource(Node source) {
         this.source = source;
     }
+    public void insert (int values){
+        this.source = this.insertRecursively(this.source, values);
+    }
+
+    private Node insertRecursively(Node newNode, int values) {
+        //If the tree is empty (root is null), create a node at the root
+        if (this.source == null){
+            Node SourceNode = new Node(values);
+            return SourceNode;
+        }
+        else
+        {
+            //
+            if (values < newNode.getvalues()){
+                newNode.setleftNode(insertRecursively(newNode.getleftNode(), values));
+
+            }
+            else{
+                newNode.setrightNode(insertRecursively(newNode.getrightNode(), values));
+            }
+            return newNode;
+        }
+    }
+
 }
