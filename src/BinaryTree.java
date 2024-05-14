82923d062c8d90b21ac56db409851574c833aad2
Thiago Vieira Ramos <thiago.vramos@hotmail.com>
2024-05-13 22:30:38 -0300

    ## New Changes - Added new methods for root query - Added new methods for searching for children on the right and children on the left.

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -2,13 +2,14 @@ public class BinaryTree {
     private Node source;
 
     public BinaryTree() {
+        this.source = null;
     }
 
     public Node getRaiz() {
         return source;
     }
 
-    public void setRaiz(Node raiz) {
+    public void setRaiz(Node source) {
         this.source = source;
     }
 }
